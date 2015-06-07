45ace51d76d69e54a1a9b112aeed01b435945ac8
haohongxian88 <1026925346@qq.com>
2015-06-07 12:54:28 +0800

    hhx-bug修改

--- Classes/gamelayer/GameUIlayer.cpp
+++ Classes/gamelayer/GameUIlayer.cpp
@@ -37,6 +37,16 @@ void GameUIlayer::changeScore(int curScore){
     m_scoreLabel->setString(__String::createWithFormat("%d",curScore)->getCString());
 //    m_scoreLabel->setString("9876543210");
 }
+void GameUIlayer::changeScoreBg(){
+    std::string str =  m_scoreLabel->getString();
+    m_scoreLabel->removeFromParentAndCleanup(true);
+    
+    Size contentSize = m_scoreBg->getContentSize();
+    m_scoreLabel = LabelAtlas::create(str, "fonts/game_frontoverhigh.png",  23.4, 28, '0');
+    m_scoreLabel->setAnchorPoint(Vec2(0.5,0.5));
+    m_scoreLabel->setPosition(Vec2(contentSize.width/2,contentSize.height/2-10));
+    m_scoreBg->addChild(m_scoreLabel);
+}
 
 
 
